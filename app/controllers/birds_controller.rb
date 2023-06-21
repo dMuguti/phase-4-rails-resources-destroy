ffcafb6977cf22c7877473513c1fe9d28c43d70f
dMuguti <omiahderek56@gmail.com>
2023-06-21 15:40:03 +0300

    Done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,17 +1,14 @@
 class BirdsController < ApplicationController
-
   # GET /birds
   def index
     birds = Bird.all
     render json: birds
   end
-
   # POST /birds
   def create
     bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
-
   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
@@ -21,7 +18,6 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
-
   # PATCH /birds/:id
   def update
     bird = Bird.find_by(id: params[:id])
@@ -32,7 +28,6 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
-
   # PATCH /birds/:id/like
   def increment_likes
     bird = Bird.find_by(id: params[:id])
@@ -44,10 +39,19 @@ class BirdsController < ApplicationController
     end
   end
 
+  def destroy
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.destroy
+      head :no_content
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
   private
 
   def bird_params
     params.permit(:name, :species, :likes)
   end
-
 end
